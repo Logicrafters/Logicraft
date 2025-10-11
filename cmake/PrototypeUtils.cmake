090ae3338ce882e64b0b6e736fd2d8b1c1380131
sharllesse <charles19lesage06@gmail.com>
2025-10-11 15:48:16 +0200

    Add prototype module and unit tests
    Adds utility CMake functions for namespaced targets and prototype library registration. Updates build configuration to support optional test building and integrates GoogleTest conditionally.

---
+++ cmake/PrototypeUtils.cmake
@@ -0,0 +1,5 @@
+﻿# Put a library name in the PROTOTYPE_LIBRARIES list which is used for the prototype unit tests executable to link the libraries.
+# The PROTOTYPE_LIBRARIES list could have other use.
+function(register_prototype_library NAME)
+    set(PROTOTYPE_LIBRARIES ${PROTOTYPE_LIBRARIES} ${NAME} CACHE INTERNAL "All libraries used by the unit tests executable in the prototype directory.")
+endfunction()
--- cmake/Utils.cmake
+++ cmake/Utils.cmake
@@ -1,4 +1,18 @@
-﻿function(add_executable_with_namespace NAMESPACE EXECUTABLE)
+﻿# Create an executable and create a alias with the designed NameSpace.
+function(add_executable_with_namespace NAMESPACE EXECUTABLE)
     add_executable(${EXECUTABLE})
     add_executable(${NAMESPACE}::${EXECUTABLE} ALIAS ${EXECUTABLE})
+endfunction()
+
+# Create a library and create a alias with the designed NameSpace.
+function(add_library_with_namespace NAMESPACE EXECUTABLE TYPE)
+    add_library(${EXECUTABLE} ${TYPE})
+    add_library(${NAMESPACE}::${EXECUTABLE} ALIAS ${EXECUTABLE})
+endfunction()
+
+# Create two variables, one named SRC_DIR containing "${CMAKE_CURRENT_SOURCE_DIR}/src".
+# And another one named INCLUDE_DIR containing "${CMAKE_CURRENT_SOURCE_DIR}/include".
+function(create_target_directory_variable)
+    set(SRC_DIR "${CMAKE_CURRENT_SOURCE_DIR}/src" PARENT_SCOPE)
+    set(INCLUDE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/include" PARENT_SCOPE)
 endfunction()
